85f718a5ca05876b7bb835bf30001a336f7dc4dd
babma04 <joaocarrilholouro@gmail.com>
2026-02-23 18:09:22 +0000

    New memory management c file for Data_Management logic

--- data_memory.py
+++ data_memory.py
@@ -1,3 +1,6 @@
+import ctypes
+import os
+
 class Data_Memory:
     """
     Simulates the full system memory with:
@@ -24,15 +27,22 @@ class Data_Memory:
     """
 
     RODATA_BASE: int = 0x500000
+    STACK_DEFAULT: int = 0x7fffffffe000
 
-    def __init__(self, memory_base: int, stack_start: int = 0x7fffffffe000):
+    def __init__(self, memory_base: int = RODATA_BASE, stack_start: int = STACK_DEFAULT):
         self.start: int= memory_base
-        # Memory as an array of 8-bit cells
-        self.memory: dict[int, bytes] = {}
-        # Stack configuration
         self.stack_start: int= stack_start     
-        self.stack_pointer: int= stack_start 
+        self.stack_pointer: int= stack_start
 
+        # Loads C memory management lib as an instance of the class
+        _base_dir = os.path.dirname(os.path.abspath(__file__))
+        _lib_path = os.path.join(_base_dir, "libmmu.so")
+        self.lib = ctypes.CDLL(_lib_path)
+
+        # Setup C types as usable types in python
+        self.lib.write_mem.argtypes = [ctypes.c_uint64, ctypes.c_uint8]
+        self.lib.read_mem.argtypes = [ctypes.c_uint64, ctypes.POINTER(ctypes.c_uint8)]
+        self.lib.read_mem.restype = ctypes.c_int
 
     # ------------------------
     # Read and write methods 
@@ -48,21 +58,26 @@ class Data_Memory:
         :return: byte values at specific addresses
         :rtype: bytes
         """
-        addr -= Data_Memory.RODATA_BASE
-        return b"".join(self.memory.get(addr + i, b"\x00") for i in range(size))
+        return bytes(self._c_read(addr + i) for i in range(size))
     
-    # Writing structure:
+    def _c_read(self, addr: int) -> int:
+        """
+        Uses the read_mem funtion from the c script to read the value from memory.
+        If the read returns 1 (signal for a misread) a MemoryError is raised.
 
-        # int value: 0x1234
-        # int size: 4
-        # bytes value: b'\34\12'
-        # bytes extended value: b'\34\12\00\00'
-
-        # writing:
-        #     @3: b'\00'
-        #     @2: b'\00'
-        #     @1: b'\12'
-        #     @0: b'\34'
+        :param addr: Virtual address to read from.
+        :type addr: int
+        :return: the integer at the given memory address
+        :rtype: int
+        :raises MemoryError: If a misread is detected
+        """
+        res = ctypes.c_uint8(0)
+        # If a misread is signaled raise MemoryError  
+        if self.lib.read_mem(addr, ctypes.byref(res)) == 1:  
+            raise MemoryError(f"Segmentation Fault at 0x{hex(addr)}")
+        # Else return the pointer's value
+        return res.value
+    
 
     def write_bytes(self, addr: int, size: int, data: bytes) -> None:
         """
@@ -75,11 +90,12 @@ class Data_Memory:
         :param data: data to write
         :type data: bytes
         """
+        # Verify data correctness 
         if not self.valid_data_length(data, size):
             data = self.get_valid_data(data, size)
-        
+        # For each byte in data write it onto sequencial addresses starting at the base address
         for i, b in enumerate(data):
-            self.memory[addr + i] = b
+            self._c_write(addr + i, b)
     
     def valid_data_length(self, data: bytes, size: int) -> bool:
         """
@@ -108,6 +124,18 @@ class Data_Memory:
         :rtype: bytes
         """
         return data[:size].ljust(size, b'\x00')
+    
+    def _c_write(self, addr: int, value: int) -> None:
+        """
+        Uses the write_mem funtion from the c script to write the value into memory.
+        Value must be 1 byte or will be truncated.
+
+        :param addr: Virtual address to write to.
+        :type addr: int
+        :param value: 1 byte value to write.
+        :type value: int
+        """
+        self.lib.write_mem(addr, value & 0xFF)
 
     # -----------------------
     #  STACK OPERATIONS (fixed 8 bytes per entry)
@@ -118,7 +146,7 @@ class Data_Memory:
         All stack cells are 8 bytes no matter what.
         
         :param value: value to add to the stack
-        :type value: int
+        :type value: bytes
         """
         # stack grows downward
         self.stack_pointer -= 0x8
@@ -133,5 +161,4 @@ class Data_Memory:
         """
         value = self.read_bytes(self.stack_pointer, 8)
         self.stack_pointer += 0x8
-        #TODO return of the object
         return value
---
+++ memory_eng.c
@@ -0,0 +1,156 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Constants declarations
+#define PAGE_SIZE 4096  // 4kb per page
+#define MAX_PAGES 512 // 512 * 8 bytes per entry = 4kb tables
+
+// Structure definition of the Table of pages of data
+typedef struct {
+    void *entries[MAX_PAGES];  // allocates 8 bytes for the address (that's why void)
+} Table;
+
+// Prototypes (Not strictly needed in this case, might remove later)
+uint8_t *decompose_address(uint64_t v_addr, int create_page);
+void write_mem(uint64_t v_addr, uint8_t value);
+int read_mem(uint64_t v_addr, uint8_t *result);
+
+// Initializing Table at NULL and rewrite it as needed using the PML4 reg
+Table* CR3 = NULL;
+
+/**
+ * Main MMU funion. 
+ * Should initialize the Table structure if it's not yet created.
+ * Used to handle reads and writes aswell as manage page creation if needed.
+ * 
+ * @param v_addr Virtual address given by the caller
+ * @param create_page Flags for the operation type (0 - for read; 1 - for write)
+ * @return Pointer to the physical byte, or NULL if unmapped and create_page is 0
+ * @note create_page param affects directly Table structures creation permission. 
+ */
+uint8_t *decompose_address(uint64_t v_addr, int create_page)
+{
+    // If the page_dir_root is not yet initialized initialize it
+    if (!CR3)
+    {
+        // If create_pages is 0 then i can't read any page in this stage and should return NULL
+        if (!create_page)
+        {
+            return NULL;
+        }
+        // Else allocates a block of memory for a Table structure and starts it out at 0
+        CR3 = (Table*)calloc(1, sizeof(Table));
+    }
+
+    // Table index and offset extration:
+    // Extracts the first 9 bits of the address
+    uint64_t pml4 = (v_addr >> 39) & 0x1FF;
+    // Extracts the 9 following bits
+    uint64_t dir_ptr = (v_addr >> 30) & 0x1FF;
+    // Extracts the 9 bits of the Directory of the Table
+    uint64_t dir = (v_addr >> 21) & 0x1FF;
+    // Ignores the last 12 bits of the address and extracts the 9 bits of the Table index
+    uint64_t table = (v_addr >> 12) & 0x1FF;
+    // Extracts the last 12 bits of the address
+    uint64_t offset = v_addr & 0xFFF;
+
+    // If the first directory level (PML64) is not initialized initializes it
+    if (!CR3->entries[pml4])
+    {
+        // If create_pages is 0 then i can't read any page in this stage and should return NULL
+        if(!create_page)
+        {
+            return NULL;
+        }
+        // Else allocates a block of memory for a Table structure and starts it out at 0
+        CR3->entries[pml4] = calloc(1, sizeof(Table));
+    }
+    // Else takes its pointer
+    Table *dir_ptr_pointer = (Table*)CR3->entries[pml4];
+
+    // If the seconds directory level (Dir_Ptr) is not yet initialized initializes it
+    if(!dir_ptr_pointer->entries[dir_ptr])
+    {
+        // If create_pages is 0 then i can't read any page in this stage and should return NULL
+        if(!create_page)
+        {
+            return NULL;
+        }
+        // Else allocates a block of memory for a Table structure and starts it out at 0
+        dir_ptr_pointer->entries[dir_ptr] = calloc(1, sizeof(Table));
+    }
+    // Else takes its pointer
+    Table *dir_pointer = (Table*)dir_ptr_pointer->entries[dir_ptr];
+
+    // If the third directory level (Dir) is not yet initialized initializes it
+    if(!dir_pointer->entries[dir])
+    {
+        // If create_pages is 0 then i can't read any page in this stage and should return NULL
+        if(!create_page)
+        {
+            return NULL;
+        }
+        // Else allocates a block of memory for a Table structure and starts it out at 0
+        dir_pointer->entries[dir] = calloc(1, sizeof(Table));
+    }
+    // Else takes its pointer
+    Table *table_pointer = (Table*)dir_pointer->entries[dir];
+
+    // If the Table is not yet initialized initializes it
+    if(!table_pointer->entries[table])
+    {
+        // If create_pages is 0 then i can't read any page in this stage and should return NULL
+        if(!create_page)
+        {
+            return NULL;
+        }
+        // Else allocates a block of memory for a Table structure and starts it out at 0
+        table_pointer->entries[table] = calloc(1, sizeof(Table));
+    }
+    // Else takes its pointer
+    uint8_t *page_addr = (uint8_t*)table_pointer->entries[table];
+
+    return page_addr + offset;
+}
+
+
+/**
+ * Writes one byte in memory.
+ * Uses the decompose method to get the real address and writes the value onto the new address.
+ * If the address returned is not NULL writes value on it
+ * 
+ * @param v_addr Virtual address given by the caller
+ * @param value 1 byte data to write
+ */
+void write_mem(uint64_t v_addr, uint8_t value)
+{
+    uint8_t *physical_addr = decompose_address(v_addr, 1);
+    if (physical_addr)
+    {
+        *physical_addr = value;
+    }
+}
+
+/**
+ * Reads one byte from memory.
+ * Uses the decompose method to get the real address and reads its value.
+ * If the address returned is NULL returns 1 to signal a Segmentation Fault
+ * 
+ * @param v_addr Virtual address given by the caller
+ * @param result Pointer to the memory block where the result is expected to be given if it is found
+ * @warning Uses 1 to signal a bad address to read and 0 to signal a success
+ */
+int read_mem(uint64_t v_addr, uint8_t *result)
+{
+    uint8_t *physical_addr = decompose_address(v_addr, 0);
+    // If the physical address could not be read because of a Segmentation Fault return 1
+    if (!physical_addr)
+    {
+        return 1;
+    }
+
+    *result = *physical_addr;
+    return 0;
+    
+}
